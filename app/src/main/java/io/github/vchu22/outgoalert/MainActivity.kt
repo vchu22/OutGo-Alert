eabf8049ee2fdc772741073aa799d4c88efc955f
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-07-04 17:51:20 -0400

    finished code for intro slider behaviors

--- app/src/main/java/io/github/vchu22/outgoalert/MainActivity.kt
+++ app/src/main/java/io/github/vchu22/outgoalert/MainActivity.kt
@@ -1,35 +1,158 @@
 package io.github.vchu22.outgoalert
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import io.github.vchu22.outgoalert.databinding.ActivityMainBinding
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    // on below line we are creating a
+    // variable for our view pager
+    lateinit var viewPager: ViewPager
+
+    // on below line we are creating a variable
+    // for our slider adapter and slider list
+    lateinit var sliderAdapter: SliderAdapter
+    lateinit var sliderList: ArrayList<SliderData>
+
+    // on below line we are creating a variable for our
+    // skip button, slider indicator text view for 3 dots
+    lateinit var skipBtn: Button
+    lateinit var indicatorSlideOneTV: TextView
+    lateinit var indicatorSlideTwoTV: TextView
+    lateinit var indicatorSlideThreeTV: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-//        val homeScreenBinding = binding.homeScreenLayout
-//        val navView: BottomNavigationView = homeScreenBinding.navView
+        // on below line we are initializing all
+        // our variables with their ids.
+        viewPager = findViewById(R.id.idViewPager)
+        skipBtn = findViewById(R.id.idBtnSkip)
+        indicatorSlideOneTV = findViewById(R.id.idTVSlideOne)
+        indicatorSlideTwoTV = findViewById(R.id.idTVSlideTwo)
+        indicatorSlideThreeTV = findViewById(R.id.idTVSlideThree)
 
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
+        // on below line we are adding click listener for our skip button
+        skipBtn.setOnClickListener {
+            // on below line we are opening a new activity
+            val i = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(i)
+        }
+
+        // on below line we are initializing our slider list.
+        sliderList = ArrayList()
+
+        // on below line we are adding data to our list
+        sliderList.add(
+            SliderData(
+                getResources().getString(R.string.intro_slide_1_title),
+                getResources().getString(R.string.intro_slide_1_text),
+                R.drawable.intro_slide_1
+            )
+        )
+
+        sliderList.add(
+            SliderData(
+                getResources().getString(R.string.intro_slide_2_title),
+                getResources().getString(R.string.intro_slide_2_text),
+                R.drawable.intro_slide_2
+            )
+        )
+
+        sliderList.add(
+            SliderData(
+                getResources().getString(R.string.intro_slide_3_title),
+                getResources().getString(R.string.intro_slide_3_text),
+                R.drawable.intro_slide_3
+            )
+        )
+
+        // on below line we are adding slider list
+        // to our adapter class.
+        sliderAdapter = SliderAdapter(this, sliderList)
+
+        // on below line we are setting adapter
+        // for our view pager on below line.
+        viewPager.adapter = sliderAdapter
+
+        // on below line we are adding page change
+        // listener for our view pager.
+        viewPager.addOnPageChangeListener(viewListener)
+
+    }
+
+    // creating a method for view pager for on page change listener.
+    var viewListener: ViewPager.OnPageChangeListener = object : ViewPager.OnPageChangeListener {
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+        }
+
+        override fun onPageSelected(position: Int) {
+            // we are calling our dots method to
+            // change the position of selected dots.
+
+            // on below line we are checking position and updating text view text color.
+            if (position == 0) {
+                indicatorSlideTwoTV.setTextColor(resources.getColor(R.color.grey))
+                indicatorSlideThreeTV.setTextColor(resources.getColor(R.color.grey))
+                indicatorSlideOneTV.setTextColor(resources.getColor(R.color.white))
+
+            } else if (position == 1) {
+                indicatorSlideTwoTV.setTextColor(resources.getColor(R.color.white))
+                indicatorSlideThreeTV.setTextColor(resources.getColor(R.color.grey))
+                indicatorSlideOneTV.setTextColor(resources.getColor(R.color.grey))
+            } else {
+                indicatorSlideTwoTV.setTextColor(resources.getColor(R.color.grey))
+                indicatorSlideThreeTV.setTextColor(resources.getColor(R.color.white))
+                indicatorSlideOneTV.setTextColor(resources.getColor(R.color.grey))
+            }
+        }
+
+        // below method is use to check scroll state.
+        override fun onPageScrollStateChanged(state: Int) {}
     }
 }
+
+//import android.os.Bundle
+//import com.google.android.material.bottomnavigation.BottomNavigationView
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.navigation.findNavController
+//import androidx.navigation.ui.AppBarConfiguration
+//import androidx.navigation.ui.setupActionBarWithNavController
+//import androidx.navigation.ui.setupWithNavController
+//import io.github.vchu22.outgoalert.databinding.ActivityMainBinding
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    private lateinit var binding: ActivityMainBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//
+////        val homeScreenBinding = binding.homeScreenLayout
+////        val navView: BottomNavigationView = homeScreenBinding.navView
+//
+////        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+////        val appBarConfiguration = AppBarConfiguration(
+////            setOf(
+////                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+////            )
+////        )
+////        setupActionBarWithNavController(navController, appBarConfiguration)
+////        navView.setupWithNavController(navController)
+//    }
+//}
