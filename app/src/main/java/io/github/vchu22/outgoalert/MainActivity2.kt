dde3231fb12df842296e1c5d2af6513999e0d072
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-07-16 22:23:59 -0400

    rename files previously associated with Dashboard to Events

--- app/src/main/java/io/github/vchu22/outgoalert/MainActivity2.kt
+++ app/src/main/java/io/github/vchu22/outgoalert/MainActivity2.kt
@@ -46,7 +46,7 @@ class MainActivity2 : AppCompatActivity() {
         // menu should be considered as top level destinations.
 //        val appBarConfiguration = AppBarConfiguration(
 //            setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+//                R.id.navigation_home, R.id.navigation_events, R.id.navigation_notifications
 //            )
 //        )
 //        setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/io/github/vchu22/outgoalert/ui/events/EventsFragment.kt
+++ app/src/main/java/io/github/vchu22/outgoalert/ui/events/EventsFragment.kt
@@ -1,4 +1,4 @@
-package io.github.vchu22.outgoalert.ui.dashboard
+package io.github.vchu22.outgoalert.ui.events
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import io.github.vchu22.outgoalert.databinding.FragmentDashboardBinding
+import io.github.vchu22.outgoalert.databinding.FragmentEventsBinding
 
-class DashboardFragment : Fragment() {
+class EventsFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentEventsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val eventsViewModel =
+            ViewModelProvider(this).get(EventsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentEventsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textEvents
+        eventsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
