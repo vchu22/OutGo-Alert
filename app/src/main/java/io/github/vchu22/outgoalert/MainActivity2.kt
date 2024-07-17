c04b00da99ca68403392c7fc9c628b31c30feb68
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-07-17 17:34:30 -0400

    configure MainActivity2 to display fragment that corresponds to current tab

--- app/src/main/java/io/github/vchu22/outgoalert/MainActivity2.kt
+++ app/src/main/java/io/github/vchu22/outgoalert/MainActivity2.kt
@@ -4,28 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import io.github.vchu22.outgoalert.databinding.ActivityMain2Binding
-
-//class MainActivity2 : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main2)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//    }
-//}
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -37,19 +21,17 @@ class MainActivity2 : AppCompatActivity() {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navView: BottomNavigationView = binding.navView
 
-//        val homeScreenBinding = binding.homeScreenLayout
-//        val navView: BottomNavigationView = homeScreenBinding.navView
-
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home, R.id.navigation_events, R.id.navigation_alerts
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_events, R.id.navigation_alerts, R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
