8a7b2438e964835181cedadeecbefcceff659f2a
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-08-15 22:40:44 -0400

    create tabbed layout for the Events fragment, tab buttons working

--- app/src/main/java/io/github/vchu22/outgoalert/ui/events/EventsFragment.kt
+++ app/src/main/java/io/github/vchu22/outgoalert/ui/events/EventsFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import io.github.vchu22.outgoalert.databinding.FragmentEventsBinding
 
 class EventsFragment : Fragment() {
@@ -16,22 +17,44 @@ class EventsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+    private lateinit var adapter: FragmentTabPageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val eventsViewModel =
-            ViewModelProvider(this).get(EventsViewModel::class.java)
-
         _binding = FragmentEventsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textEvents
-        eventsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        tabLayout = binding.tabLayout
+        viewPager = binding.viewPager
+        adapter = FragmentTabPageAdapter(childFragmentManager, lifecycle)
+        tabLayout.addTab(tabLayout.newTab().setText("List View"))
+        tabLayout.addTab(tabLayout.newTab().setText("Calendar"))
+        viewPager.adapter = adapter
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
         return root
     }
 
