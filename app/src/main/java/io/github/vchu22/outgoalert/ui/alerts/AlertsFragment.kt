d0ba955190ac0b98718ffd882f31017050d22c4a
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-09-03 16:05:44 -0400

    create main page layout for Alerts tab

--- app/src/main/java/io/github/vchu22/outgoalert/ui/alerts/AlertsFragment.kt
+++ app/src/main/java/io/github/vchu22/outgoalert/ui/alerts/AlertsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import io.github.vchu22.outgoalert.databinding.FragmentAlertsBinding
@@ -28,10 +27,6 @@ class AlertsFragment : Fragment() {
         _binding = FragmentAlertsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAlerts
-        alertsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
